5014b252bb5423ff671ff5e229bbbfadbd9f02d0
yangzhendong <yangzd1993@foxmail.com>
2021-01-23 16:02:12 +0800

    [Feature]通用工具类

--- common-utils/src/test/java/com/rude3knife/commonutils/CommonUtilsApplicationTests.java
+++ common-utils/src/test/java/com/rude3knife/commonutils/CommonUtilsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.rude3knife.commonutils;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.rude3knife.commonutils.utils.IPAddressUtil;
+import org.junit.Test;
 
-@SpringBootTest
-class CommonUtilsApplicationTests {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CommonUtilsApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void isInRange() {
+        assertTrue(IPAddressUtil.isInRange("192.168.1.5", "192.168.0.0/23"));
+        assertTrue(IPAddressUtil.isInRange("192.168.1.5", "192.168.1.0/24"));
+        assertFalse(IPAddressUtil.isInRange("192.168.1.5", "192.168.1.0/32"));
+        assertTrue(IPAddressUtil.isInRange("10.249.30.53", "10.249.0.0/16"));
+        assertFalse(IPAddressUtil.isInRange("10.249.30.53", "10.249.0.0/24"));
+        assertFalse(IPAddressUtil.isInRange("10.249.30.53", "10.249.0.0/32"));
     }
 
 }
